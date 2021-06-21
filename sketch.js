636ec58cfe359902e7fc92be89275dab9c72819b
priyapandey2020 <66728335+priyapandey2020@users.noreply.github.com>
2021-06-21 13:21:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var satelitte, satelitteImg;
+var earth, earthImg;
+var groundStationLeft, groundStationRight;
+var groundStationLeftImg, groundStationRightImg;
+var upLinkLeft, upLinkLeftImg;
+
+function preload(){
+  
+}
+
+function setup(){
+  createCanvas(700,500);
+  
+  
+}
+
+function draw(){
+  background("black")
+  
+  
+  
+  drawSprites();
+}
